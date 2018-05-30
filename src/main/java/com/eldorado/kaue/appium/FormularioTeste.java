9a48a07eab2c0bd1bfe269500aef719bfd9e86f5
Kaue Pereira <kauefontes@outlook.com>
2018-05-30 01:43:05 -0400

    switch and checkbox interactions added S3A16

--- src/main/java/com/eldorado/kaue/appium/FormularioTeste.java
+++ src/main/java/com/eldorado/kaue/appium/FormularioTeste.java
@@ -50,27 +50,58 @@ public class FormularioTeste {
 		desiredCapabilities.setCapability("platformName", "Android");
 		desiredCapabilities.setCapability("deviceName", "NZAA470170");
 		desiredCapabilities.setCapability("automationName", "uiautomator2");
-	    //desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\kaue.pereira\\eclipse-workspace\\AppiumTeste\\src\\main\\resources\\CTAppium-1-1.apk");
-	    desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\kauef\\eclipse-workspace\\appiumSample\\src\\main\\resources\\CTAppium-1-1.apk");
-	    
-	    AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    
-	    //Selecionar formulario
-	    List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
-	    elementosEncontrados.get(1).click();
-	    
-	    //clicar no combo
-	    driver.findElement(MobileBy.AccessibilityId("console")).click();
-	    
-	    //selecionar a opcao desejada
-	    driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='PS4']")).click();
-	    
-	    //verificar a opcao selecionada
-	    String text = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
-	    Assert.assertEquals("PS4", text);
-	    	    
-	    driver.quit();
+		//desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\kaue.pereira\\eclipse-workspace\\AppiumTeste\\src\\main\\resources\\CTAppium-1-1.apk");
+		desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\kauef\\eclipse-workspace\\appiumSample\\src\\main\\resources\\CTAppium-1-1.apk");
+		
+		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//Selecionar formulario
+		List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
+		elementosEncontrados.get(1).click();
+		
+		//clicar no combo
+		driver.findElement(MobileBy.AccessibilityId("console")).click();
+		
+		//selecionar a opcao desejada
+		driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='PS4']")).click();
+		
+		//verificar a opcao selecionada
+		String text = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
+		Assert.assertEquals("PS4", text);
+		
+		driver.quit();
+	}
+	@Test
+	public void deveInteragirSwitchCheckBox() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("platformName", "Android");
+		desiredCapabilities.setCapability("deviceName", "NZAA470170");
+		desiredCapabilities.setCapability("automationName", "uiautomator2");
+		//desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\kaue.pereira\\eclipse-workspace\\AppiumTeste\\src\\main\\resources\\CTAppium-1-1.apk");
+		desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\kauef\\eclipse-workspace\\appiumSample\\src\\main\\resources\\CTAppium-1-1.apk");
+		
+		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//Selecionar formulario
+		driver.findElement(By.xpath("//*[@text='Formulário']")).click();
+		
+		//verificar status dos elementos
+		MobileElement check = driver.findElement(By.className("android.widget.CheckBox"));
+		MobileElement switc = driver.findElement(MobileBy.AccessibilityId("switch"));
+		Assert.assertTrue(check.getAttribute("checked").equals("false"));
+		Assert.assertTrue(switc.getAttribute("checked").equals("true"));
+		
+		//clicar nos elementos
+		check.click();
+		switc.click();
+		
+		//verificar status alterados
+		Assert.assertFalse(check.getAttribute("checked").equals("false"));
+		Assert.assertFalse(switc.getAttribute("checked").equals("true"));
+		
+		driver.quit();
 	}
 
 
