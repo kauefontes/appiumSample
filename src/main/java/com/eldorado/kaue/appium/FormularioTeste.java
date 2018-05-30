22873d1b8b6b02b6a9b1674c817da0304084959f
Kaue Pereira <kauefontes@outlook.com>
2018-05-29 22:19:21 -0400

    chalange initiated, assert not done yet S3A16

--- src/main/java/com/eldorado/kaue/appium/FormularioTeste.java
+++ src/main/java/com/eldorado/kaue/appium/FormularioTeste.java
@@ -72,6 +72,7 @@ public class FormularioTeste {
 		
 		driver.quit();
 	}
+	
 	@Test
 	public void deveInteragirSwitchCheckBox() throws MalformedURLException {
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
@@ -102,6 +103,40 @@ public class FormularioTeste {
 		Assert.assertFalse(switc.getAttribute("checked").equals("true"));
 		
 		driver.quit();
+	}
+	
+	@Test
+	public void interactionsAndFill() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("platformName", "Android");
+		desiredCapabilities.setCapability("deviceName", "NZAA470170");
+		desiredCapabilities.setCapability("automationName", "uiautomator2");
+		//desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\kaue.pereira\\eclipse-workspace\\AppiumTeste\\src\\main\\resources\\CTAppium-1-1.apk");
+		desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\kauef\\eclipse-workspace\\appiumSample\\src\\main\\resources\\CTAppium-1-1.apk");
+		
+		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
+		elementosEncontrados.get(1).click();
+		
+		String name = "kaue";
+		//interactions
+		driver.findElement(By.className("android.widget.EditText")).sendKeys(name);
+		driver.findElement(By.className("android.widget.CheckBox")).click();
+		driver.findElement(MobileBy.AccessibilityId("switch")).click();
+		
+		List<MobileElement> botoesEncontrados = driver.findElements(By.className("android.widget.Button"));
+		botoesEncontrados.get(0).click();
+		
+		
+		
+		//asserts
+		//Assert.assertEquals("Nome: "+ name, text);
+		
+		driver.quit();
+		
+		
 	}
 
 
